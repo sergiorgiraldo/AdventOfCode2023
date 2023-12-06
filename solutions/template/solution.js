b1cf9c75884a78b83f8d7625f2c0866c0e4d892f
Sergio Giraldo <sergio@giraldo.com.br>
2023-12-06 16:08:37 +0100

    chore: store execution times ::by sergio giraldo @ 20231206T1608CET, gpg signed

--- solutions/template/solution.js
+++ solutions/template/solution.js
@@ -4,54 +4,57 @@ const readline = require("readline");
 const fs = require("fs");
 const fromHere = position(__dirname);
 fs.unlinkSync(path.join(__dirname, "answer.txt"));
-var streamAnswer = fs.createWriteStream(path.join(__dirname, "answer.txt"), {flags:"a"});
+var streamAnswer = fs.createWriteStream(path.join(__dirname, "answer.txt"), {
+	flags: "a"
+});
 const report = (...messages) => {
-    streamAnswer.write(JSON.stringify(messages) + "\n");
-    console.log(`[${require(fromHere("../../package.json")).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages);
-  };
+	streamAnswer.write(JSON.stringify(messages) + "\n");
+	console.log(
+		`[${require(fromHere("../../package.json")).logName} / ${__dirname
+			.split(path.sep)
+			.pop()}]`,
+		...messages
+	);
+};
 const lib = require("../lib/dayTODO");
 
- function run () {
-    solveForFirstStar();
-    solveForSecondStar();
+function run() {
+	solveForFirstStar();
+	solveForSecondStar();
 }
 
- function solveForFirstStar () {
-  const start = Date.now();
-  const filePath = path.join(__dirname, "input.txt");
-  const readInterface = readline.createInterface({
-      input: fs.createReadStream(filePath)
-  });
-  
-  let result=0;
+function solveForFirstStar() {
+	const start = Date.now();
+	const filePath = path.join(__dirname, "input.txt");
+	const readInterface = readline.createInterface({
+		input: fs.createReadStream(filePath)
+	});
 
-  readInterface.on("line", function (line) {
+	let result = 0;
 
-  });
-  
-  readInterface.on("close", function () {
-    const end = Date.now();
-    report("Solution 1:", result, ` Execution time: ${end - start} ms`);
-  });
+	readInterface.on("line", function (line) {});
+
+	readInterface.on("close", function () {
+		const end = Date.now();
+		report("Solution 1:", result, ` Execution time: ${end - start} ms`);
+	});
 }
 
- function solveForSecondStar () {
-  const start = Date.now();
-  const filePath = path.join(__dirname, "input.txt");
-  const readInterface = readline.createInterface({
-      input: fs.createReadStream(filePath)
-  });
+function solveForSecondStar() {
+	const start = Date.now();
+	const filePath = path.join(__dirname, "input.txt");
+	const readInterface = readline.createInterface({
+		input: fs.createReadStream(filePath)
+	});
 
-  let result=0;
+	let result = 0;
 
-  readInterface.on("line", function (line) {
+	readInterface.on("line", function (line) {});
 
-  });
-  
-  readInterface.on("close", function () {
-    const end = Date.now();
-    report("Solution 2:", result, ` Execution time: ${end - start} ms`);
-  });
+	readInterface.on("close", function () {
+		const end = Date.now();
+		report("Solution 2:", result, ` Execution time: ${end - start} ms`);
+	});
 }
 
 run();
