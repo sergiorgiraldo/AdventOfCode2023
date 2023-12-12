892ec94f74eed0660e2d90820aac3a1ac1097a38
Sergio Giraldo <sergio@giraldo.com.br>
2023-12-12 13:06:53 +0100

    feat!: day twelve completed ::by sergio giraldo @ 20231212T1306CET, gpg signed

--- solutions/lib/helpers.js
+++ solutions/lib/helpers.js
@@ -1,5 +1,28 @@
 const gcd = (x, y) => (!y ? x : gcd(y, x % y)); // recursive, get remainder until it is 0
 
-const lcm = (x, y) => (x * y) / gcd(x, y); 
+const lcm = (x, y) => (x * y) / gcd(x, y);
 
-module.exports = { gcd, lcm };
+function memoize(func) {
+	let stored = new Map();
+    
+	return function () {
+		var kwargs = [];
+		for (var i = 0; i < arguments.length; i++) {
+			kwargs[i] = arguments[i];
+		}
+
+		var current = JSON.stringify(kwargs);
+
+		if (stored.has(current)) {
+			return stored.get(current);
+		} else {
+			const result = func.apply(void 0, kwargs);
+
+			stored.set(current, result);
+			
+            return result;
+		}
+	};
+}
+
+module.exports = { gcd, lcm, memoize };
